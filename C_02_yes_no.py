14c39aba3483bf414b9961064da2111face86c0a
Nicke-lee <159388972+Nicke-lee@users.noreply.github.com>
2024-02-16 11:50:58 +1300

    fixed yes/ no error msg, created int checker > 13

--- C_02_yes_no.py
+++ C_02_yes_no.py
@@ -10,3 +10,9 @@ def yes_no(question):
         else:
             print("You did not choose a valid response")
 
+
+want_instructions = yes_no("Do you want to read the instructions? ")
+print(f"You chose {want_instructions}")
+
+roll_again = yes_no("Do you want to roll again? ")
+print(f"You said {roll_again} to rolling again. ")
